119934839dbb32a4bd9b15c29dbe3cc32ec1acb8
kevin FERRANDON <kferrandon@gmail.com>
2022-02-01 11:56:35 +0100

    demo

--- cv.php
+++ cv.php
@@ -1,5 +1,3 @@
-<h1>CV</h1>
-<div>
-    Ici mon cv
+<div class="row">
+Ici mon super cv...
 </div>
-
--- menu.php
+++ menu.php
@@ -1,11 +1,15 @@
-<ul class="nav">
+  <!-- Content here -->
+  <ul class="nav">
   <li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="index.php?page=accueil">Accueil</a></a>
+    <a class="nav-link active" aria-current="page" href="index.php?page=accueil">Accueil</a>
   </li>
-   <li class="nav-item"><a class="nav-link" href="index.php?page=presentation">Presentation</a></li>
-        <li class="nav-item" ><a class="nav-link" href="index.php?page=apropos">A propos</a></li>
-        <li class="nav-item"><a class="nav-link" href="index.php?page=cv">CV</a></li>
-
-    </ul>
-</nav>
-
+  <li class="nav-item">
+    <a class="nav-link" href="index.php?page=contact">Contact</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="index.php?page=competences">Competences</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="index.php?page=cv">Cv</a>
+  </li>
+</ul>
